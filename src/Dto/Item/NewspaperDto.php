3ddf3545ff0d50524b0dd731743de7655e63cd39
tacman <tacman@users.noreply.github.com>
2026-09-22 12:06:19 +0000

    NewspaperDto: add digitizedIn facet (where a title can be read online)

--- src/Dto/Item/NewspaperDto.php
+++ src/Dto/Item/NewspaperDto.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Survos\DataContracts\Dto\Item;
 
 use Survos\DataContracts\Metadata\ContentType;
+use Survos\FieldBundle\Attribute\Field;
 use Survos\SchemaOrgBundle\Attribute\SchemaOrg;
 
 #[SchemaOrg('Newspaper')]
@@ -16,6 +17,16 @@ class NewspaperDto extends AbstractWorkDto
     public ?string $edition      = null;
     /** Has searchable full-text pages */
     public bool    $hasTranscription = false;
+
+    /**
+     * Where this title can be read online, e.g. chronicling-america, virginia-chronicle; empty for
+     * a title known only from a directory. A title-level fact set by catalog surveys (harvest's
+     * survey:newspapers); issue and page rows leave it null.
+     *
+     * @var list<string>|null
+     */
+    #[Field(facet: true, filterable: true, group: 'Availability')]
+    public ?array $digitizedIn = null;
 
     public static function contentType(): string { return ContentType::NEWSPAPER; }
 }
